f93fe52e747f9294bc5d452376b73a2e23dd599c
Ayush <aayush199813@gmail.com>
2023-02-16 17:49:03 +0530

    Sending pdffile  through api call

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-require('dotenv').config();
 const cors = require('cors');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env['PORT'] || 11000;
+var pdf = require('html-pdf');
+
 const corsOptions = {
   origin: '*',
   credentials: true,
@@ -12,7 +14,31 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+app.get('/getInvoice', (req, res) => {
+    pdf.create(`<h1>Hey My pdf </h1>`).toStream((err, pdfStream) => {
+      if (err) {
+        // handle error and return a error response code
+        console.log(err);
+        return res.sendStatus(500);
+      } else {
+        // send a status code of 200 OK
+        res.statusCode = 200;
+        // once we are done reading end the response
+        pdfStream.on('end', () => {
+          // done reading
+          return res.end();
+        });
+        // pipe the contents of the PDF directly to the response
+        pdfStream.pipe(res);
+      }
+    });
+});
 
+app.get('/', (req, res) => {
+  res.status(200).send({
+    msg: 'Hi there, welcome to Invoice API. Go to /sample route to get sample data',
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
